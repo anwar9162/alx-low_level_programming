bf8c07e9dd3fff2343a0b654349e360598f22751
anwar9162 <anwarahmed9162@gmail.com>
2022-12-11 11:10:44 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+* main - print the string in the put function
  *
- *
- *
- * Description: using the main function
- *
- * this program prints "Programming is like building a multilingual puzzle
- *
- *  Return: 0
- *
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*
+*/
 
-return (0);
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
