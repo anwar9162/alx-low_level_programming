996a4c880989e502af721128ee9777ad7e0bcd4f
anwar9162 <anwarahmed9162@gmail.com>
2022-12-18 08:26:52 -0800

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,20 +12,13 @@ char letter; /*Decarling statement*/
 /*for letter*/
 
 for (letter = 'a'; letter <= 'z'; letter++)
-
 {
-
-	if (letter == 'q') /*continue if is q*/
-
+if (letter == 'q') /*continue if is q*/
 		continue;
-
 	else if (letter == 'e')/*continue if is q*/
-
 		continue;
-
 	putchar(letter);/*print letter*/
 }
-
 putchar('\n'); /*new line*/
 
 return (0);
